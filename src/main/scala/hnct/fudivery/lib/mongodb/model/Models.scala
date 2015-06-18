026685fbb936c4fe6e917a5338d7308941f613f7
Cuong Truong <cuong.truong@channelpilot.com>
2015-06-18 17:37:28 +0200

    dev

--- src/main/scala/hnct/fudivery/lib/mongodb/model/Models.scala
+++ src/main/scala/hnct/fudivery/lib/mongodb/model/Models.scala
@@ -6,13 +6,14 @@ import com.mongodb.util.JSON
 import com.mongodb.DBObject
 import com.mongodb.casbah.commons.MongoDBObject
 import org.bson.types.ObjectId
+import org.json4s.DefaultFormats
 
 /**
  * @author tduccuong
  */
 
 trait Serializable {
-  private val formats = Serialization.formats(NoTypeHints)
+  private val formats = DefaultFormats //Serialization.formats(NoTypeHints)
   
   def toJson = Serialization.writePretty(this)(formats)
   
@@ -26,12 +27,14 @@ object ModelBuilder {
   // this query will always prepended to any query in our system to ensure that only models of the current version will be loaded
   val MODEL_QUERY = MongoDBObject("modver" -> MODEL_VERSION)
   
-  implicit val formats = Serialization.formats(NoTypeHints)
+  implicit val formats = DefaultFormats //Serialization.formats(NoTypeHints)
   
   def fromJson[T](json: String)(implicit m: Manifest[T]) = Serialization.read[T](json)
   
   def fromDbObject[T](dbo: DBObject)(implicit m: Manifest[T]) = fromJson[T](dbo.toString())
 }
+
+/* --------------------------------------- Item model --------------------------------------- */
 
 case class ItemE(
   modver: String,
@@ -40,7 +43,11 @@ case class ItemE(
   ingrds: Seq[String],
   photos: Seq[String],
   desc: String,
-  feedbacks: Seq[Tuple3[String, String, Double]]
+  feedbacks: Seq[Tuple3[String, String, Double]], // Seq{(feedbackId, userId, rankingScore)}
+  foodTypes: Seq[Tuple2[String, String]], // Seq{(foodTypeName, foodTypeId)}
+  foodCats: Seq[Tuple2[String, String]], // Seq{(foodCatName, foodCatId)}
+  restaurant: Tuple5[String, String, Double, Double, String], // (resName, resId, (lat, lon), resAddr)
+  disProgs: Seq[Tuple2[String, String]] // Seq{(progName, progId)}
 ) extends Serializable
 
 object ItemE {
@@ -49,10 +56,34 @@ object ItemE {
     ingrds: Seq[String],
     photos: Seq[String],
     desc: String,
-    feedbacks: Seq[Tuple3[String, String, Double]]
+    feedbacks: Seq[Tuple3[String, String, Double]],
+    foodTypes: Seq[Tuple2[String, String]],
+    foodCats: Seq[Tuple2[String, String]],
+    restaurant: Tuple5[String, String, Double, Double, String],
+    disProgs: Seq[Tuple2[String, String]]
   ) = new ItemE(
     ModelBuilder.MODEL_VERSION,
     new ObjectId().toString,
-    name, ingrds, photos, desc, feedbacks
+    name, ingrds, photos, desc, feedbacks, foodTypes, foodCats, restaurant, disProgs
+  )
+}
+
+/* --------------------------------------- Ingredient model --------------------------------------- */
+
+case class IngredientE(
+  modver: String,
+  _id: String, 
+  name: String, 
+  desc: String
+) extends Serializable
+
+object IngredientE {
+  def apply(
+    name: String, 
+    desc: String
+  ) = new IngredientE(
+    ModelBuilder.MODEL_VERSION,
+    new ObjectId().toString,
+    name, desc
   )
 }
--- src/test/scala/hnct/fudivery/lib/mongodb/ConnectionTest.scala
+++ src/test/scala/hnct/fudivery/lib/mongodb/ConnectionTest.scala
@@ -21,21 +21,36 @@ object ConnectionTest extends App {
   
   val item = ItemE(
     "abc1", 
-    Vector("1", "2", "3"),
-    Vector("url1", "url2"),
+    Seq("1", "2", "3"),
+    Seq("url1", "url2"),
     "Desc",
-    Vector(
+    Seq(
       ("fb1", "uid1", 0.2),
       ("fb1", "uid1", 0.3)
+    ),
+    Seq(
+      ("ft1", "id1"),
+      ("ft2", "id2")
+    ),
+    Seq(
+      ("fc1", "id1"),
+      ("fc2", "id2")
+    ),
+    ("resName", "resId", 100.0, 100.0, "resAddr"),
+    Seq(
+      ("p1", "id1"),
+      ("p2", "id2")
     )
   )
-  val col = mongoDb.useCol[ItemE] 
-  col.insert(item.toDbObject)
-  
-  val query = MongoDBObject()
-  val all = mongoDb.query[ItemE](query)
-  for (doc <- all) {
-    val item = ModelBuilder.fromDbObject[ItemE](doc) 
-    Console.println(item.toJson)
-  }
+  println(item.toJson)
+//  val col = mongoDb.useCol[ItemE] 
+//  col.insert(item.toDbObject)
+//  
+//  val query = MongoDBObject()
+//  val all = mongoDb.query[ItemE](query)
+//  for (doc <- all) {
+//    Console.println(doc)
+//    val item = ModelBuilder.fromDbObject[ItemE](doc) 
+//    Console.println(item.toJson)
+//  }
 }
