d5fb616b973c9bb9c1f4530617ee56a65ff51895
Cuong Truong <cuong.truong@channelpilot.com>
2015-06-18 11:39:32 +0200

    dev

--- src/main/scala/hnct/fudivery/lib/mongodb/model/Models.scala
+++ src/main/scala/hnct/fudivery/lib/mongodb/model/Models.scala
@@ -1,9 +1,35 @@
 package hnct.fudivery.lib.mongodb.model
 
+import org.json4s.jackson.Serialization
+import org.json4s.NoTypeHints
+import com.mongodb.util.JSON
+import com.mongodb.DBObject
+
 /**
  * @author tduccuong
  */
 
-case class BaseE(_id: String)
+trait Serializable {
+  private val formats = Serialization.formats(NoTypeHints)
+  
+  def toJson = Serialization.write(this)(formats)
+  
+  def toDbObject = JSON.parse(toJson).asInstanceOf[DBObject]
+}
 
-case class AbcE(name: String, ingrds: Seq[String])
+object ModelBuilder {
+  implicit val formats = Serialization.formats(NoTypeHints)
+  
+  def fromJson[T](json: String)(implicit m: Manifest[T]) = Serialization.read[T](json)
+  
+  def fromDbObject[T](dbo: DBObject)(implicit m: Manifest[T]) = fromJson[T](dbo.toString())
+}
+
+case class ItemE(
+  _id: String, 
+  name: String, 
+  ingrds: Seq[String],
+  photos: Seq[String],
+  desc: String,
+  feedbacks: Seq[Tuple3[String, String, Double]]
+) extends Serializable
--- src/test/scala/hnct/fudivery/lib/mongodb/ConnectionTest.scala
+++ src/test/scala/hnct/fudivery/lib/mongodb/ConnectionTest.scala
@@ -7,7 +7,8 @@ import hnct.fudivery.lib.mongodb.model.ItemE
 import com.owlike.genson._
 import org.json4s.jackson.Serialization
 import org.json4s.NoTypeHints
-import hnct.fudivery.lib.mongodb.model.AbcE
+import hnct.fudivery.lib.mongodb.model._
+import org.bson.types.ObjectId
 
 /**
  * @author tduccuong
@@ -21,19 +22,23 @@ object ConnectionTest extends App {
   val colTest = mongoDb.useColl(classOf[ItemE])
   
   implicit val formats = Serialization.formats(NoTypeHints)
-  val abc1 = AbcE("abc1", Vector("1", "2", "3"))
-  val s = Serialization.write(abc1)
-  println(s)
+  val abc1 = ItemE(
+    new ObjectId().toString(), 
+    "abc1", 
+    Vector("1", "2", "3"),
+    Vector("url1", "url2"),
+    "Desc",
+    Vector(
+      ("fb1", "uid1", 0.2),
+      ("fb1", "uid1", 0.3)
+    )
+  )
   
-  val abc2 = Serialization.read[AbcE](s)
-  println(Serialization.write(abc2))
+  colTest.insert(abc1.toDbObject)
   
-//  colTest.insert(item1.toDbObject())
-//  
-//  val all = colTest.find()
-//  for (doc <- all) {
-//    Console.println(doc.toString())
-//    val item = EntityE.fromDbObject(doc, classOf[ItemE]).asInstanceOf[ItemE] 
-//    Console.println(item.toJson)
-//  }
+  val all = colTest.find()
+  for (doc <- all) {
+    val item = ModelBuilder.fromDbObject[ItemE](doc) 
+    Console.println(item.toJson)
+  }
 }
