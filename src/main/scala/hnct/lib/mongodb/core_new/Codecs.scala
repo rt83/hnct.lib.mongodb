afbe0290057e14d5dea3186b0b71ccc1f03fc325
ryan <truongducthang@jayeson.com.sg>
2016-11-09 23:02:38 +0800

    Add codec file. Remove configuration in build file for macros.

---
+++ src/main/scala/hnct/lib/mongodb/core_new/Codecs.scala
@@ -0,0 +1,11 @@
+package hnct.lib.mongodb.core_new
+
+/**
+  * Created by ryan on 11/9/2016.
+  *
+  * Provide methods to read primitive and certain scala data structure like set, array, list
+  *
+  */
+object Codecs {
+
+}
