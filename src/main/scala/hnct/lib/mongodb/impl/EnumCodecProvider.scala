1bd4f24baf0175b7001f9c06566f9e0004792c4c
ryan <truongducthang@jayeson.com.sg>
2018-02-26 09:01:11 +0800

    General java enum codec provider (usage can be found in hnct.firegate.homepage)

---
+++ src/main/scala/hnct/lib/mongodb/impl/EnumCodecProvider.scala
@@ -0,0 +1,32 @@
+package hnct.lib.mongodb.impl
+
+import org.bson.{BsonReader, BsonWriter}
+import org.bson.codecs.{Codec, DecoderContext, EncoderContext}
+import org.bson.codecs.configuration.{CodecProvider, CodecRegistry}
+
+object EnumCodecProvider {
+  def create[E <: Enum[E]](c : Class[E]) = new CodecProvider {
+
+    override def get[T](clazz: Class[T], registry: CodecRegistry): Codec[T] = {
+
+      val codec = if (c == clazz) {
+
+        println("OKOK\n\n\n")
+
+        new Codec[E] {
+          override def encode(writer: BsonWriter, value: E, encoderContext: EncoderContext): Unit = {
+            writer.writeString(value.toString())
+          }
+
+          override def getEncoderClass: Class[E] = c
+
+          override def decode(reader: BsonReader, decoderContext: DecoderContext): E = {
+            return Enum.valueOf(c, reader.readString())
+          }
+        }
+      } else null
+
+      codec.asInstanceOf[Codec[T]]
+    }
+  }
+}
